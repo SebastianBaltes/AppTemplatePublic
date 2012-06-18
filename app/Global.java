446ebe0b0e6faea10bc3521704f23066eec68028
greenteafreak <user@localhorst>
2012-06-18 15:19:23 +0200

    no use for a message

--- app/Global.java
+++ app/Global.java
@@ -1,17 +1,23 @@
+import com.avaje.ebean.Ebean;
+
 import play.Application;
 import play.GlobalSettings;
 import play.Logger;
-
-
+import play.Play;
 
 public class Global extends GlobalSettings {
 
 	@Override
 	public void onStart(final Application app) {
 		Logger.debug("onStart()");
+		setupLogging(app);
+		
 	}
-	
-	
-	
-	
+
+	private void setupLogging(final Application app) {
+		if (Play.isDev() || Play.isTest()) {
+			Ebean.getServer(null).getAdminLogging().setDebugGeneratedSql(true);
+		}
+	}
+
 }
