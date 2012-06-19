7901bf4b98cb7178ff22a9cd0bf36282e40baeda
greenteafreak <user@localhorst>
2012-06-19 20:34:07 +0200

    cleaned up

--- app/authenticate/admin/AdminSecured.java
+++ app/authenticate/admin/AdminSecured.java
@@ -1,6 +1,5 @@
 package authenticate.admin;
 
-
 import models.User;
 import controllers.admin.routes;
 import play.mvc.Result;
@@ -8,22 +7,19 @@ import play.mvc.Http.Context;
 import authenticate.AbstractSecured;
 import authenticate.Authenticated;
 
-
 public class AdminSecured extends AbstractSecured {
-	
+
 	@Override
 	public String getUsername(final Context ctx) {
-		final String userId = super.getUsername(ctx); 
-		if (userId == null) return null;
 		final User loggedinUser = Authenticated.getAuthenticatedUser(ctx.session());
-		if (loggedinUser != null && loggedinUser.getRole().isAdminRole()) return userId;
+		if (loggedinUser != null && loggedinUser.getRole().isAdminRole()) return String.valueOf(loggedinUser.getId());
 		return null;
-	}	
-	
+	}
+
 	@Override
 	public Result onUnauthorized(final Context _ctx) {
 		super.onUnauthorized(_ctx);
 		return redirect(routes.LoginController.index());
 	}
-	
+
 }
