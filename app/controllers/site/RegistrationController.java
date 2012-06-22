cf0aa90f762ab7e33bfb916795d8c87337447a55
greenteafreak <user@localhorst>
2012-06-22 13:11:38 +0200

    changed registration flow

--- app/controllers/site/RegistrationController.java
+++ app/controllers/site/RegistrationController.java
@@ -3,6 +3,7 @@ package controllers.site;
 import models.Role;
 import models.User;
 
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
 
@@ -41,15 +42,19 @@ public class RegistrationController extends Controller {
 		final RegistrationForm myForm = requestForm.get();
 		Logger.debug("RegistrationController: got form=" + myForm);
 
-		// FIXME: send success message even in this case and notify the existing
-		// user
-		// by mail about this incident.
+		//send success message even in this case and notify the existing
+		// user by mail about this incident.
 		final User existingUser = User.findByEmail(myForm.getMandatory().getEmail());
 		if (existingUser != null) {
-			flash().put(FlashScope.ERROR, "Die Email-Adresse ist schon vergeben!");
-			return badRequest(views.html.site.registration.render(requestForm));
-//			flash().put(FlashScope.SUCCESS, "Registrierung erflogreich ! Bitte überprüfen Sie Ihr EMailpostfach.");
-//			return ok(views.html.site.registration.render(requestForm));
+			try {
+				sendDoubleRegistrationNotificationMail(existingUser);
+				flash().put(FlashScope.SUCCESS, "Registrierung erflogreich ! Bitte überprüfen Sie Ihr EMailpostfach.");
+			} catch (final EmailException e) {
+				Logger.error("Could not send DoubleRegistrationNotificationMail confirmation mail due to =" + e, e);
+				flash().put(FlashScope.ERROR,
+						"Es ist ein Fehler bei der Registrierung aufgetreten, bitte versuchen Sie es in wenigen Augenblicken erneut!");
+			}
+			return ok(views.html.site.registration.render(requestForm));
 		}
 
 		if (!myForm.getMandatory().isPasswordMatching()) {
@@ -94,6 +99,16 @@ public class RegistrationController extends Controller {
 		return ok(views.html.site.registration.render(requestForm));
 	}
 	
+	private static void sendDoubleRegistrationNotificationMail(final User user) throws EmailException{
+		final Email mail = MailHelper.createSimpleMail();
+		mail.addTo(user.getEmail());
+		mail.setSubject("Doppelte Anmeldung");
+		mail.setMsg(views.html.email.doubleRegistrationNotificationMail_text.render(user).toString());
+		
+		Logger.info("about to send DoubleRegistrationNotificationMail=" + ReflectionToStringBuilder.toString(mail));
+		mail.send();
+	}
+
 	public static Result activate(final String _email, final String _hash) {
 		final User user = User.findByEmail(_email);
 		if (user == null) {
