433a0ce404f309d2a2cabff8e6babb1f6e5924c9
Sebastian Baltes <baltes@objectcode.de>
2012-06-27 11:59:24 +0200

    ImageUpload

--- app/controllers/admin/DbImageController.java
+++ app/controllers/admin/DbImageController.java
@@ -1,19 +1,11 @@
 package controllers.admin;
 
-import java.awt.Dimension;
-import java.awt.Image;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import javax.imageio.ImageIO;
-
 import models.DbImage;
-import net.coobird.thumbnailator.Thumbnails;
+import models.RawImage;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
@@ -27,34 +19,19 @@ import play.mvc.Controller;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
-import eu.medsea.mimeutil.MimeUtil;
 
 public class DbImageController extends Controller {
 
-	static {
-		MimeUtil.registerMimeDetector("eu.medsea.mimeutil.detector.MagicMimeMimeDetector");
-	}
-
-	public static Result thumbnail(long id) {
-		Logger.info("############### thumbnail("+id+")");
-		DbImage image = DbImage.find.byId(id);
-		if (image == null) {
+	public static Result image(Long id) {
+		RawImage rawImage = RawImage.byId(id);
+		if (rawImage == null) {
 			return status(404);
 		}
-		return ok(image.getThumbnail()).as(image.getThumbnailMimetype());
-	}
-
-	public static Result image(long id) {
-		Logger.info("############### thumbnail("+id+")");
-		DbImage image = DbImage.find.byId(id);
-		if (image == null) {
-			return status(404);
-		}
-		return ok(image.getImage()).as(image.getMimetype());
+		return ok(rawImage.getImage()).as(rawImage.getMimetype());
 	}
 
 	/**
-	 * @return json with {"id":id} of the newly created or updated image
+	 * @return json with all infos of the newly created or updated dbImage
 	 */
 	public static Result upload() {
 		try {
@@ -71,83 +48,67 @@ public class DbImageController extends Controller {
 			image.setLabel(filePart.getFilename());
 			File imgFile = filePart.getFile();
 			byte[] bytes = FileUtils.readFileToByteArray(imgFile);
-			image.setMimetype(getMimeType(bytes, filePart.getContentType()));
-			setImageWithSizeAndThumbnail(image, bytes);
+			setRawImage(image, bytes);
+			setRawThumbnail(image);
 			image.saveOrUpdate();
 			ObjectNode result = dbImageToJson(image);
 			return ok(result);
 		} catch (Exception e) {
+			Logger.warn(e.getMessage(),e);
 			return status(400, e.getMessage());
 		}
 	}
 
+	private static void setRawImage(DbImage image, byte[] bytes) {
+		RawImage rawImage = image.getImage();
+		if (rawImage==null) {
+			rawImage = new RawImage();
+			image.setImage(rawImage);
+		}
+		rawImage.setImage(bytes);
+	}
+
+	private static void setRawThumbnail(DbImage image) {
+		Configuration config = Play.application().configuration();
+		int thumbnailWidth = config.getInt("thumbnails.width");
+		int thumbnailHeight = config.getInt("thumbnails.height");
+		RawImage thumbnail = image.getThumbnail();
+		if (thumbnail==null) {
+			thumbnail = new RawImage();
+			image.setThumbnail(thumbnail);
+		}
+		byte[] thumbnailBytes = image.getImage().createThumbnail(thumbnailWidth, thumbnailHeight);
+		thumbnail.setImage(thumbnailBytes);
+	}
+	
 	private static ObjectNode dbImageToJson(DbImage image) {
 		ObjectNode result = Json.newObject();
 		result.put("id", image.getId());
 		result.put("filename", image.getFilename());
 		result.put("label", image.getLabel());
-		result.put("width", image.getWidth());
-		result.put("height", image.getHeight());
-		result.put("mimetype", image.getMimetype());
-		result.put("thumbnailWidth", image.getThumbnailWidth());
-		result.put("thumbnailHeight", image.getThumbnailHeight());
-		result.put("thumbnailMimetype", image.getMimetype());
+		result.put("image", rawImageToJson(image.getImage()));
+		result.put("thumbnail", rawImageToJson(image.getThumbnail()));
 		return result;
 	}
 	
 	
-	public static void setImageWithSizeAndThumbnail(DbImage image, byte[] bytes) throws IOException {
-		image.setImage(bytes);
-		sizeFromImage(image);
-		setThumbnailFromImage(image);
+	private static ObjectNode rawImageToJson(RawImage image) {
+		ObjectNode result = Json.newObject();
+		result.put("id", image.getId());
+		result.put("width", image.getWidth());
+		result.put("height", image.getHeight());
+		result.put("mimetype", image.getMimetype());
+		return result;
 	}
-
-	private static void setThumbnailFromImage(DbImage image) throws IOException {
-		Configuration config = Play.application().configuration();
-		int thumbnailWidth = config.getInt("thumbnails.width");
-		int thumbnailHeight = config.getInt("thumbnails.height");
-		ByteArrayOutputStream out = new ByteArrayOutputStream(thumbnailWidth*thumbnailHeight*4);
-		Thumbnails.of(new ByteArrayInputStream(image.getImage())).size(thumbnailWidth, thumbnailHeight).outputFormat("png").toOutputStream(out);
-		image.setThumbnail(out.toByteArray());
-		sizeFromThumbnail(image);
-	}
-
-	private static void sizeFromImage(DbImage image) throws IOException {
-		Dimension d = dimension(image.getImage());
-		image.setWidth(d.width);
-		image.setHeight(d.height);
-	}
-	
-	private static void sizeFromThumbnail(DbImage image) throws IOException {
-		Dimension d = dimension(image.getThumbnail());
-		image.setThumbnailWidth(d.width);
-		image.setThumbnailHeight(d.height);
-	}
-
-	private static Dimension dimension(byte[] bytes) throws IOException {
-		Image img = ImageIO.read(new ByteArrayInputStream(bytes));
-		return new Dimension(img.getWidth(null),img.getHeight(null));
-	}	
 
 	private static Long getId(MultipartFormData body) {
 		Map<String, String[]> params = body.asFormUrlEncoded();
 		String[] ids = params.get("id");
-		String id_ = ids == null || ids.length == 0 ? null : ids[0];
+		String id_ = (ids == null || ids.length == 0) ? null : ids[0];
 		if (StringUtils.isEmpty(id_)) {
 			return null;
 		}
 		return Long.valueOf(id_);
-	}
-
-	/**
-	 * paranoid - don't trust the transmitted mime type
-	 */
-	private static String getMimeType(byte[] bytes, String mimeType) {
-		Collection<?> mimeTypes = MimeUtil.getMimeTypes(bytes);
-		if (mimeTypes == null || mimeTypes.isEmpty()) {
-			return mimeType;
-		}
-		return mimeTypes.iterator().next().toString();
 	}
 
 }
---
+++ app/models/RawImage.java
@@ -0,0 +1,137 @@
+package models;
+
+import java.awt.Image;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Collection;
+
+import javax.imageio.ImageIO;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Version;
+
+import net.coobird.thumbnailator.Thumbnails;
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
+import eu.medsea.mimeutil.MimeUtil;
+
+@SuppressWarnings("serial")
+@Entity
+public class RawImage extends Model {
+
+	static {
+		MimeUtil.registerMimeDetector("eu.medsea.mimeutil.detector.MagicMimeMimeDetector");
+	}
+	
+	private static Finder<Long, RawImage> finder = new Finder<Long, RawImage>(Long.class, RawImage.class);
+	
+    @Id
+    public Long id;
+    @Version
+    public Timestamp lastUpdate;
+	@Lob
+	@Required
+	public byte[] image;
+	@Required
+	public int width;
+	@Required
+	public int height;
+	@Required
+	public String mimetype;	
+	
+	public static RawImage byId(Long id) {
+		return finder.byId(id);
+	}
+	
+	public void setImage(byte[] bytes) {
+		try {
+			Image img = ImageIO.read(new ByteArrayInputStream(bytes));
+			this.width = img.getWidth(null);
+			this.height = img.getHeight(null);
+			this.mimetype = mimetype(bytes);
+			this.image = bytes;
+		} catch (IOException e) {
+			throw new RuntimeException(e.getMessage(),e);
+		}
+	}
+	
+	public byte[] createThumbnail(int thumbnailWidth, int thumbnailHeight) {
+		ByteArrayOutputStream out = new ByteArrayOutputStream(thumbnailWidth*thumbnailHeight*4);
+		try {
+			Thumbnails.of(new ByteArrayInputStream(getImage())).size(thumbnailWidth, thumbnailHeight).outputFormat("png").toOutputStream(out);
+		} catch (IOException e) {
+			throw new RuntimeException("can't create thumbnail",e);
+		}
+		byte[] thumbbytes = out.toByteArray();
+		return thumbbytes; 
+	}
+
+	/**
+	 * paranoid - don't trust any transmitted mime type
+	 */
+	private static String mimetype(byte[] bytes) {
+		Collection<?> mimeTypes = MimeUtil.getMimeTypes(bytes);
+		if (mimeTypes == null || mimeTypes.isEmpty()) {
+			throw new RuntimeException("unknown mimetype");
+		}
+		return mimeTypes.iterator().next().toString();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public Timestamp getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public String getMimetype() {
+		return mimetype;
+	}
+
+	@Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+		RawImage other = (RawImage) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+	@Override
+	public String toString() {
+		return "RawImage [id=" + id + ", width=" + width + ", height=" + height + ", mimetype=" + mimetype + "]";
+	}
+    
+}
